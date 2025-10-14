53e3bbe8a03eb52bfec3be943685e08b29d951e1
Chris Battis <chris.battis@gmail.com>
2025-10-14 10:05:09 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.simpleicons.org', 'logo.clearbit.com'],
+  },
+}
+
+module.exports = nextConfig
